d9b295b043d8d32be7056bc205de05ce36430834
Sterling <sterlingbarton81@gmail.com>
2023-07-27 19:02:09 -0600

    projects section established with initial styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,103 +4,128 @@ import Image from 'next/image';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
 import defaultHeroGuy1 from '../images/hero-guy-1.png'
 import lgHeroGuy1 from '../images/hero-guy-1-lg.png'
 import html5 from '../images/html5.png'
 import css from '../images/css.png'
 import flask from '../images/flask.png'
 import javascript from '../images/javascript.png'
-import next from '../images/nextjs.png'
+import next from '../images/nextjs.webp'
 import node from '../images/node.png'
 import sql from '../images/sql.png'
 import python from '../images/python.png'
 import react from '../images/react.png'
+import taskPlanner from '../images/taskPlanner.png'
+import CLI from '../images/CLI.png'
+import colorPicker from '../images/colorPicker.png'
 import styles from '../styles/page.module.css'
 
 
 export default function Home() {
   return (
     <main>
-      <Box id='home' style={{position: 'relative'}}>
-        <Box className={styles.introSection}>
+      <Box id='home' component='section' className={styles.introSection}>
         <Box className={styles.heading}>
           <Typography gutterBottom variant="h2" className={styles.introTitle}>Hello, I'm Sterling</Typography>
           <Typography gutterBottom variant="h3" className={styles.introDesc}>Software Engineer ...</Typography>
         </Box>
         <Box className={styles.heroGuy1}>
-          <Image className={styles.heroGuy1} height={190} width={'100%'} src={defaultHeroGuy1} alt='hero guy 1'></Image>
+          <Image className={styles.heroGuy1} height={180} width={'100%'} src={defaultHeroGuy1} alt='hero guy 1'></Image>
           {/* <Image className={styles.heroGuy1__lg} height={400} width={'100%'} src={lgHeroGuy1} alt='hero guy 1'></Image> */}
-        </Box>
         </Box>
       </Box>
       <Box id='skills' component='section' className={styles.skillsSection}>
         <Typography gutterBottom variant='h4' className={styles.skillsTitle}>Technologies I work with</Typography>
+        <Box className={styles.hr}></Box>
         <Box className={styles.skillsContainer}>
             <Box className={styles.skills}>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={html5} alt='html5 logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={html5} alt='html5 logo'/></Box>
             </Box>
             <Box className={styles.skills}>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={css} alt='css logo'/></Box>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={javascript} alt='javascript logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={css} alt='css logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={javascript} alt='javascript logo'/></Box>
             </Box>
             <Box className={styles.skills}>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={python} alt='python logo'/></Box>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={sql} alt='sql logo'/></Box>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={flask} alt='flask logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={python} alt='python logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={sql} alt='sql logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={70} width={60} src={flask} alt='flask logo'/></Box>
             </Box>
             <Box className={styles.skills}>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={react} alt='react logo'/></Box>
-              <Box className={styles.skillsItem}><Image height={50} width={50} src={next} alt='nextjs logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={react} alt='react logo'/></Box>
+              <Box className={styles.skillsItem}><Image height={60} width={60} src={next} alt='nextjs logo'/></Box>
             </Box>
             <Box className={styles.skills}>
-            <Box className={styles.skillsItem}><Image height={50} width={50} src={node} alt='nodejs logo'/></Box>
+            <Box className={styles.skillsItem}><Image height={60} width={60} src={node} alt='nodejs logo'/></Box>
             </Box>
         </Box>
      </Box>
-     <Box id='projects'>
-        <Typography variant='h3'>Projects</Typography>
-        <Grid>
-            <Grid>
-                <Typography variant='p'>Task Planner</Typography>
-                <Box
+     <Box id='projects' component='section' className={styles.projectsSection}>
+        <Typography gutterBottom variant='h3' className={styles.projectsTitle}>My Projects</Typography>
+        <Box className={styles.hr}></Box>
+        <Grid className={styles.projectsGrid}>
+            <Grid className={styles.projectsRow}>
+              <Box className={styles.projectInfo}>
+                <Typography variant='h6'>Task Planner</Typography>
+                <Typography variant='p'>Hello this is garbage rn</Typography>
+                <Button variant='contained'>GitHub</Button>
+                <Button variant='contained'>Live site</Button>
+
+              </Box>
+                <Box id={styles.imageWrapper1} className={styles.imageWrapper}>
+                  <Image
+                      component="img"
+                      height={230}
+                      width={360}
+                      sx={{
+                      maxHeight: { xs: 233, md: 167 },
+                      maxWidth: { xs: 350, md: 250 },
+                      }}
+                      alt="The house from the offer."
+                      src={taskPlanner}
+                  />
+                </Box>
+            </Grid>
+            <Grid className={styles.projectsRow}>
+              <Box id={styles.imageWrapper2} className={styles.imageWrapper}>
+                <Image
                     component="img"
+                    height={275}
+                      width={250}
+                      sx={{
+                      maxHeight: { xs: 233, md: 167 },
+                      maxWidth: { xs: 350, md: 250 },
+                      }}
+                      alt="Python CLI game"
+                      src={CLI}
+                />
+              </Box>
+              <Box className={styles.projectInfo}>
+                <Typography variant='h6'>CLI Maze Game</Typography>
+                <Typography variant='p'>Hello this is garbage rn</Typography>
+                <Button variant='contained'>GitHub</Button>
+              </Box>
+            </Grid>
+            <Grid className={styles.projectsRow}>
+              <Box className={styles.projectInfo}>
+                <Typography variant='h6'>Color Picker</Typography>
+                <Typography variant='p'>Hello this is garbage rn</Typography>
+                <Button variant='contained'>GitHub</Button>
+                <Button variant='contained'>Live site</Button>
+              </Box>
+                <Box id={styles.imageWrapper3} className={styles.imageWrapper}>
+                <Image
+                    component="img"
+                    height={200}
+                    width={230}
                     sx={{
-                    height: 150,
-                    width: 175,
                     maxHeight: { xs: 233, md: 167 },
                     maxWidth: { xs: 350, md: 250 },
                     }}
-                    alt="The house from the offer."
-                    src="https://www.recipetineats.com/wp-content/uploads/2020/09/Vanilla-Cupcakes-with-Vanilla-Swiss-Meringue-SQ.jpg"
+                    alt="Color Picker"
+                    src={colorPicker}
                 />
-            </Grid>
-            <Grid>
-                <Box
-                    component="img"
-                    sx={{
-                        height: 150,
-                        width: 175,
-                        maxHeight: { xs: 233, md: 167 },
-                        maxWidth: { xs: 350, md: 250 },
-                    }}
-                    alt="The house from the offer."
-                    src="https://www.recipetineats.com/wp-content/uploads/2020/09/Vanilla-Cupcakes-with-Vanilla-Swiss-Meringue-SQ.jpg"
-                />
-                <Typography variant='p'>CLI Maze Game</Typography>
-            </Grid>
-            <Grid>
-                <Typography variant='p'>Color Picker</Typography>
-                <Box
-                    component="img"
-                    sx={{
-                        height: 150,
-                        width: 175,
-                        maxHeight: { xs: 233, md: 167 },
-                        maxWidth: { xs: 350, md: 250 },
-                    }}
-                    alt="The house from the offer."
-                    src="https://www.recipetineats.com/wp-content/uploads/2020/09/Vanilla-Cupcakes-with-Vanilla-Swiss-Meringue-SQ.jpg"
-                />
+              </Box>
             </Grid>
         </Grid>
     </Box>
